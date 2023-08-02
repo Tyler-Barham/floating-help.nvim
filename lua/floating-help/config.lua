402d7514f5599cd08ca39cd6360669d3935167b8
Tyler Barham <tyler.r.barham@gmail.com>
2023-08-02 12:19:31 +1200

    Update default config

--- lua/floating-help/config.lua
+++ lua/floating-help/config.lua
@@ -3,10 +3,9 @@ local M = {}
 M.namespace = vim.api.nvim_create_namespace('FloatingHelp')
 
 local defaults = {
-  max_width = 80,   -- Whole numbers are columns
-  max_height = 0.9, -- Decimals are percentage of editor
-  position = 'C',   -- NW,N,NW,W,C,E,SW,S,SE (C==center)
-  query = '',       -- Stores the search query
+  max_width = 80,   -- Whole numbers are columns/rows
+  max_height = 0.9, -- Decimals are a percentage of the editor
+  position = 'E',   -- NW,N,NW,W,C,E,SW,S,SE (C==center)
 }
 
 M.options = {}
